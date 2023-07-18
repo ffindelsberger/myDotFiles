10d4634c47fcac71a4c3d2df28db33ced313ef69
ffindelsberger <lolfindinger@gmail.com>
2023-07-18 10:09:58 +0200

    add floaterm

--- .config/nvim/lua/custom/plugins/floaterm.lua
+++ .config/nvim/lua/custom/plugins/floaterm.lua
@@ -2,7 +2,7 @@ return {
   {
     'voldikss/vim-floaterm',
     config = function()
-      vim.keymap.set('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 fish <CR> ",
+      vim.keymap.set('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.9 --width=0.8 --autoclose=2 fish <CR> ",
         { desc = 'Open Floating Term' })
       vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>", { desc = 'Floaterm toggle' })
       vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>", { desc = 'close Floaterm' })
