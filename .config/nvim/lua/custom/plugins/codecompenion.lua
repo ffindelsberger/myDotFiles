c0daf9a9b0ed5c7b1bc695ea4f4a0c8eeae80ba6
Florian Findelsberger <florianfindelsberger@gmail.com>
2025-10-15 11:23:11 +0200

    codecompanion

--- .config/nvim/lua/custom/plugins/codecompenion.lua
+++ .config/nvim/lua/custom/plugins/codecompenion.lua
@@ -7,10 +7,23 @@ return {
 					adapter = "anthropic"
 				}
 			},
-			opts = {
-				log_level = "TRACE"
-			},
 			adapters = {
+				openai = function()
+					return require("codecompanion.adapters").extend("openai", {
+						env = {
+							api_key =
+							""
+						}
+					})
+				end,
+				anthropic = function()
+					return require("codecompanion.adapters").extend("anthropic", {
+						env = {
+							api_key =
+							""
+						}
+					})
+				end
 			}
 		},
 		dependencies = {
@@ -18,12 +31,7 @@ return {
 			"nvim-treesitter/nvim-treesitter",
 		},
 		keys = {
-			{ "<leader>cc", function() vim.cmd('CodeCompanionChat toggle') end, desc = "harpoon to file 1", },
+			{ "<leader>cc", function() vim.cmd('CodeCompanionChat toggle') end, desc = "Open/Close codecompanion Chat", },
 		},
 	},
 }
-
-
--- vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
--- vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
--- vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
