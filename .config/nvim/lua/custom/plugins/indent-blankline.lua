aea9c8a2a1c145c538b101f726548b07ba2f4c5b
Florian Findelsberger <florianfindelsberger@gmail.com>
2026-02-03 11:10:23 +0100

    indent-blankline

--- .config/nvim/lua/custom/plugins/indent-blankline.lua
+++ .config/nvim/lua/custom/plugins/indent-blankline.lua
@@ -1,20 +1,24 @@
 return {
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    main = "ibl",
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- :help ibl.setup()
-    opts = {},
-    config = function()
-      require("ibl").setup {
-        indent = {
-          char = '┊',
-        },
-        whitespace = {
-          remove_blankline_trail = true,
-        },
-      }
-    end
-  },
+	{
+		-- Add indentation guides even on blank lines
+		'lukas-reineke/indent-blankline.nvim',
+		main = "ibl",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- :help ibl.setup()
+		opts = {},
+		config = function()
+			require("ibl").setup {
+				indent = {
+					char = '┊',
+				},
+				scope = {
+					show_start = false,
+					show_end = true
+				},
+				whitespace = {
+					remove_blankline_trail = true,
+				},
+			}
+		end
+	},
 }
