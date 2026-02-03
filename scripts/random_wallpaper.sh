adcf9370ad786fe67c2fe5946c11bc0845e5165d
Florian Findelsberger <florianfindelsberger@gmail.com>
2026-02-03 11:11:48 +0100

    random-wallapper script

--- scripts/random_wallpaper.sh
+++ scripts/random_wallpaper.sh
@@ -1,8 +1,9 @@
-WALLPAPER_DIR="$HOME/data/images/wallpapers/"
+WALLPAPER_DIR="$HOME/data/git/myDotFiles/images/wallpapers/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+hyprctl hyprpaper wallpaper DP-1, "$WALLPAPER"
+hyprctl hyprpaper wallpaper DP-2, "$WALLPAPER"
