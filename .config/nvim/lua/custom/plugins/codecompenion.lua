952f022978d85e31b7076d25922fb16465189271
Florian Findelsberger <florianfindelsberger@gmail.com>
2026-02-19 12:44:28 +0100

    code companion

--- .config/nvim/lua/custom/plugins/codecompenion.lua
+++ .config/nvim/lua/custom/plugins/codecompenion.lua
@@ -1,2 +1,47 @@
 return {
+	{
+		"olimorris/codecompanion.nvim",
+		opts = {
+			interactions = {
+				chat = {
+					adapter = "anthropic",
+					model = "claude-opus-4-5"
+				}
+			},
+			adapters = {
+				http = {
+					openai = function()
+						return require("codecompanion.adapters").extend("openai", {
+							env = {
+								api_key =
+								""
+							}
+						})
+					end,
+					anthropic = function()
+						return require("codecompanion.adapters").extend("anthropic", {
+							env = {
+								api_key =
+								""
+							},
+							{
+								schema = {
+									model = {
+										default = "claude-opus-4-5"
+									}
+								}
+							}
+						})
+					end
+				}
+			}
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		keys = {
+			{ "<leader>cc", function() vim.cmd('CodeCompanionChat adapter=anthropic model=claude-opus-4-5 toggle') end, desc = "Open/Close codecompanion Chat", },
+		},
+	},
 }
